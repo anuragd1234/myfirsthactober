cc08e709ffbf40963cd6c0866953a33b62a2fc4e
Jenil Nagrecha <75077531+Jenilnagrecha@users.noreply.github.com>
2022-10-25 22:41:35 +0530

    Create code.cpp

---
+++ code.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+#define ll long long
+#define int long long
+#define fr(i, n) for (int i = 0; i < n; i++)
+
+using namespace std;
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n, x;
+    cin >> n >> x;
+    int n;
+    vector<int> l(n), r(n), c(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> l[i] >> r[i] >> c[i];
+    }
+
+    return 0;
+}
